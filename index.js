7e63c9fb1dd9bff5d7924a1438378237267c0e45
Megha Jibhakate <106766775+meghajibhakate@users.noreply.github.com>
2023-01-14 19:06:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+    var hamburger=document.getElementById("drop")
+function myFunction() {
+    // var hamburger=document.getElementById("drop")
+   if (hamburger.style.display === "block") {  
+    // document.getElementById("icon").style.display = "none"; 
+    hamburger.style.display = "none";        
+   }   
+   else {
+       hamburger.style.display = "block";
+   }
+}
+
+
+
+// if(hamburger=="block"){
+//     document.getElementById("icon").style.display = "none";
+//     // document.getElementById("cross-icon").style.display = "block";
+    
+// }
+// else{
+//     document.getElementById("cross-icon").style.display = "none"; 
+// }
