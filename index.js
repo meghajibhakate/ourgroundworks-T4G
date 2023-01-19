405a4c62febdb84bedb2db7bb9211384450d657e
Megha Jibhakate <106766775+meghajibhakate@users.noreply.github.com>
2023-01-19 09:49:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 
-    var hamburger=document.getElementById("drop")
+var hamburger = document.getElementById("drop")
 function myFunction() {
     // var hamburger=document.getElementById("drop")
-   if (hamburger.style.display === "block") {  
-    // document.getElementById("icon").style.display = "none"; 
-    hamburger.style.display = "none";        
-   }   
-   else {
-       hamburger.style.display = "block";
-   }
+    if (hamburger.style.display === "block") {
+        // document.getElementById("icon").style.display = "none"; 
+        hamburger.style.display = "none";
+    }
+    else {
+        hamburger.style.display = "block";
+    }
 }
 
 
-// var hamburger=document.getElementById("drop")
-// function myFunctionAbout() {
-//     // var hamburger=document.getElementById("drop")
-//    if (hamburger.style.display === "block") {  
-//     // document.getElementById("icon").style.display = "none"; 
-//     hamburger.style.display = "none";        
-//    }   
-//    else {
-//        hamburger.style.display = "block";
-//    }
-// }
+
 
 
 
