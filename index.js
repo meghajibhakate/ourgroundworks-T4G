294041ef00035944f44c5b1b2b4c008c723d6512
meghajibhakate <meghajibhakate18@gmail.com>
2023-01-27 09:34:04 +0530

    html alt text

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var hamburger = document.getElementById("drop")
 function myFunction() {
     if (hamburger.style.display == "block") {
    
-
         hamburger.style.display = "none";
     }
     else {
